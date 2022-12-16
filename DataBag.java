0823e661a41f00051568ce5e9897d12e34bc2514
Samnang Thorn <83148244+SamnangTsn@users.noreply.github.com>
2022-12-15 21:54:38 -0500

    add javadoc

--- DataBag.java
+++ DataBag.java
@@ -1,19 +1,27 @@
 import java.util.Random;
 
+/**
+ * DataBag that generate random data object.
+ * @author Samnang_Thorn
+ *
+ */
 public class DataBag {
 
 	private int data;
 	
-	//constructor
+	/**
+	 * Constructor.
+	 */
 	public DataBag() {
 		Random obj = new Random();
 		this.data = obj.nextInt(1000);
 	}
 	
-	//getter
+	/**
+	 * Getter method.
+	 * @return data contain within the object.
+	 */
 	public int getData() {
 		return this.data;
-	}
-	
-	
+	}	
 }
