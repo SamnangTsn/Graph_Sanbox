a8d9b59db10d37dee50239742e24b9afcaa2c556
Samnang Thorn <83148244+SamnangTsn@users.noreply.github.com>
2022-12-29 14:29:39 -0500

    add buttonLayout.java

---
+++ ButtonLayout.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ButtonLayout extends JPanel{
+	
+	public ButtonLayout(){
+		this.setBackground(Color.GRAY);
+		//this.setPreferredSize(new Dimension(500, 200));
+		this.setBounds(0, 500, 500, 200);;
+	}
+
+}
