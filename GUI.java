ea92a529cd123d8fdb7d2d926165a6fae7313af2
Samnang Thorn <83148244+SamnangTsn@users.noreply.github.com>
2022-12-29 14:31:11 -0500

    add parameter graph for layout access

--- GUI.java
+++ GUI.java
@@ -4,14 +4,18 @@ import javax.swing.JFrame;
 public class GUI extends JFrame{
 	
 	GraphLayout layout1;
+	ButtonLayout layout2;
 	
-	public GUI(){
+	public GUI(Graphs graph){
 		
 		this.setTitle("Graph_Sandbox");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 700);
-		layout1 = new GraphLayout();
+		this.setLayout(null);
+		layout1 = new GraphLayout(graph);
 		this.add(layout1);
+		layout2 = new ButtonLayout();
+		this.add(layout2);
 		this.setVisible(true);
 	}
 
