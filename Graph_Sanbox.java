1ef0c0f29751acc28ab25041f196354bc83bd3d0
Samnang Thorn <83148244+SamnangTsn@users.noreply.github.com>
2022-12-29 13:56:33 -0500

    add graph_sanbox.java

---
+++ Graph_Sanbox.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Samnang_Thorn
+ *
+ */
+public class Graph_Sanbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Graphs graph = new Graphs(true);
+		graph.printGraph();
+		graph.printGraphDetail();
+		
+		new GUI();
+
+		
+	}
+
+}
